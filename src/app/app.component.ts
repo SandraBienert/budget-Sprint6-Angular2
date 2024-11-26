09cbb639f73735ed9779c98f31bbcfc34ca03083
SandraBienert <sandrabienert@outlook.com>
2024-11-26 17:18:01 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BudgetsListComponent } from "./components/budgets-list/budgets-list.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [BudgetsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
